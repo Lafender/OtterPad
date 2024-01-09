01f82dc0e9f68a7b945bd803156110b0e2f47d62
Laffy <11316682+Lafender@users.noreply.github.com>
2024-01-09 05:23:41 -0500

    first working scripts uploaded
    fully functional. these scripts test a basic function of javascript functionality (function-ability) within a given web-browser.

---
+++ Gadgets/hello.js
@@ -0,0 +1,9 @@
+/*
+	this timer was created by grepper, tutplus, stackoverflow, and w3 schools, digitalocean
+	and Lafender
+*/
+// install helloViewName variable
+var helloViewName = "helloView";
+// add id=helloView; "<p>" element
+document.getElementsByTagName("body")[0].innerHTML="<p id=" + helloViewName + "> Hello! </p>";
+console.log("Body and helloView loaded");
